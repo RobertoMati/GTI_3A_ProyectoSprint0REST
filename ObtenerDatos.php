7c7f1502f9bfc3c1661d9c6168277df94f544055
Roberto Matilla Augustinus <robertomatillaaugustinus@gmail.com>
2022-10-07 01:18:49 +0200

    Añado el código necesario en este archivo para que funcione.

---
+++ ObtenerDatos.php
@@ -0,0 +1,19 @@
+<?php
+
+//Incluimos el archivo de conexión a la base de datos
+include 'bbdd.php';
+
+//Obtener la información de la tabla de la base de datos
+$consulta = "SELECT * FROM datos";
+//Ejecutamos la consulta
+$resultado = mysqli_query($conexion, $consulta);
+
+//Creamos un array para almacenar los datos
+//Mientras haya datos en la tabla, los almacenamos en el array
+while($fila = mysqli_fetch_array($resultado)){
+    //Almacenamos los datos en el array
+    $datos[] = array(
+        'dato' => $fila['dato'],
+    );
+}
+?>
