76cdc4520b70c6bc137feec5b7c99a71af83f94c
Roberto Matilla Augustinus <robertomatillaaugustinus@gmail.com>
2022-10-14 14:30:02 +0200

    Archivo importar base de datos

---
+++ bd/sprint0.sql
@@ -0,0 +1,35 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 14-10-2022 a las 10:38:03
+-- Versión del servidor: 10.4.24-MariaDB
+-- Versión de PHP: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `sprint0`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `medidas`
+--
+
+CREATE TABLE `medidas` (
+  `uuid` text NOT NULL,
+  `minor` int(100) NOT NULL,
+  `fecha` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
